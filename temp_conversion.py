afb3fac8f705bbcdecaf6dd94a33638458355e0e
Wesley Brown <wesleykebrown@yahoo.com>
2023-09-05 22:20:59 -0400

    Completed Assignment

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,36 +1,42 @@
 
 def convert_100_to_celsius():
-    # Convert a temperature of 100 degrees fahrenheit to celsius
-    # Save this to a variable called celsius_100, and use print() to print out the value
-    # Is the resulting temperature you get an integer or float?
-    # Print 'float' if it is a float or 'int' if it is an int
-    # How do you know? Write a comment below your code explaining how you know which it is
+    celsius_100 = ((100 - 32) * 5/9)
+    print(celsius_100)
+    print("float")
+    # This is a float as there are decimal values calculated in the formula.
 
-#convert_100_to_celsius()
+convert_100_to_celsius()
 
 def convert_0_to_celsius():
-    # Convert a temperature of 0 degrees fahrenheit to celsius
-    # Save this to a variable called celsius_0, and use print() to print out the value
+    celsius_0 = ((0 - 32) * 5/9)
+    print(celsius_0)
 
-#convert_0_to_celsius()
+convert_0_to_celsius()
 
 def convert_34_2_to_celsius():
-    # Convert a temperature of 34.2 degrees fahrenheit to celsius
+    print((34.2 - 32) * 5/9)
     # Do this one all in one print statement without saving any variables
     
-#convert_34_2_to_celsius()
+convert_34_2_to_celsius()
 
 
 # Now, can you convert back?
 
 
 def convert_5_to_fahrenheit():
-    # Convert a temperature of 5 degrees celsius to fahrenheit and print it out
+    fahrenheit_5 = ((5 * 9/5) + 32)
+    print(fahrenheit_5)
 
-#convert_5_to_fahrenheit()
+convert_5_to_fahrenheit()
 
 def hotter_temp():
-    # What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
-    # Print out the hotter temp: '30.2 degrees celsius' or '85.1 degrees fahrenheit', respectively
+    #Converted 85.1 fahrenheit to celsius
+    #celsius_85_1 = ((85.1 - 32) * 5/9)
+    #Created list of temperatures
+    #list_of_temperatures = [30.2, celsius_85_1]
+    #Printed a statement showing the max temperature in the list
+    #print(max(list_of_temperatures))
+    #What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
+    print("30.2 degrees celsius")
 
-#hotter_temp()
+hotter_temp()
